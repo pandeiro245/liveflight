aff9e8cf7f26f5cf83831d6f69af92ba11331986
Satoru Mikami <saicologic@gmail.com>
2015-03-01 12:19:31 +0900

    Add place controller

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,2 @@
 class Place < ActiveRecord::Base
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :places
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :welcome
   root 'welcome#index'
